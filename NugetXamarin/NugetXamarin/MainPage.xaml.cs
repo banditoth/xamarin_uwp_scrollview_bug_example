5d6f9d44c51c16a21b7b3480d38c1a78f20d7c7f
Toth Andras BDP FIIW42 <andras.toth3@zf.com>
2021-12-20 13:20:27 +0100

    Add project files.

---
+++ NugetXamarin/NugetXamarin/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NugetXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainPageViewModel();
+
+            Random rand = new Random();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                TapGestureRecognizer rec = new TapGestureRecognizer()
+                {
+                    NumberOfTapsRequired = 1,
+                    Command = new Command(() => DisplayAlert("Tapped", i.ToString(), "Ok"))
+                };
+
+                BoxView view = new BoxView()
+                {
+                    HorizontalOptions = LayoutOptions.Fill,
+                    HeightRequest = rand.Next(0, 350),
+                    Color = Color.FromRgb(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255))
+                };
+
+                view.GestureRecognizers.Add(rec);
+
+                container.Children.Add(view);
+            }
+        }
+
+
+    }
+}
---
+++ NugetXamarin/NugetXamarin/MainPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NugetXamarin
+{
+    public class MainPageViewModel
+    {
+        public DateTime Date
+        {
+            get
+            {
+                return new DateTime(1996, 11, 26, 10, 25, 36);
+            }
+        }
+    }
+}
